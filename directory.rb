c115bae27956a9b3452c6eb3b2564075fade8e8c
Volker Bergner <volker@bergner.one>
2019-08-13 19:30:24 +0000

    INteractive menu added

--- directory.rb
+++ directory.rb
@@ -11,7 +11,32 @@ students = [
   {name: "The Joker", cohort: :november}, 
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november} 
-]
+] 
+
+def interactive_menu 
+  students = [] 
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students" 
+    puts "2. Show the students" 
+    puts "9. Exit" 
+    # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection 
+      when "1" 
+        students = input_students 
+      when "2" 
+        print_header 
+        print(students) 
+        print_footer(students)
+      when "9" 
+        exit # this will cause the program to terminate 
+      else puts "I don't know what you meant, try again"
+    end 
+  end
+end
 
 def input_students 
   puts "Please enter the names of the students" 
@@ -48,7 +73,8 @@ def print_footer(names)
 end
 
 #nothing happens until we call the methods
-students = input_students
-print_header 
-print(students)
-print_footer(students)
+# students = input_students
+# print_header 
+# print(students)
+# print_footer(students)
+interactive_menu
